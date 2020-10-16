675729e9dc50753107ddb38383d8dfd313d16bde
Alfonso Mateo <alfonso.mateo@tadaima.studio>
2020-10-17 01:42:19 +0200

    web-rdn initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './app/App.vue'
+import './registerServiceWorker'
+import router from './app/router'
+import store from './app/store'
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
---
+++ src/server.js
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// Middlewares
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('port', process.env.PORT || 3000)
+
+app.get('/', function(req, res) {
+  console.log('Example app listening on port' + ' ' + app.get('port'))
+  res.send('Example app listening on port' + ' ' + app.get('port'))
+})
+
+// Database connection --- dbName
+const uri = 'mongodb://localhost:27017/dbName'
+const options = { useNewUrlParser: true, useCreateIndex: true }
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    console.log('Connected to DB')
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+// Middleware for Vue.js router's history mode
+const history = require('connect-history-api-fallback')
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), function() {
+  console.log('Example app listening on port' + ' ' + app.get('port'))
+})
