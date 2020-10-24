c684ed59d999977ca7045b2fd533f56eb2a2ae3a
Alfonso Mateo <alfonso.mateo@tadaima.studio>
2020-10-24 14:55:10 +0200

    feature/WRN-heroku-deploy: added env variables and seed removed

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -18,15 +20,16 @@ app.use(express.static(path.join(__dirname, 'public')))
 const memberRoutes = require('./routes/member')
 app.use('/api/member', memberRoutes)
 
-app.set('port', process.env.PORT || 3000)
+const hostname = process.env.HOST
+const port = process.env.PORT
 
 app.get('/', function(req, res) {
   console.log('get / is working fine')
-  res.send('Example app listening on port' + ' ' + app.get('port'))
+  res.send('Example app listening on port' + ' ' + port)
 })
 
 // Database connection --- wrdn
-const uri = 'mongodb://localhost:27017/wrdn'
+const uri = `mongodb://${hostname}:27017/wrdn`
 const options = { useNewUrlParser: true, useCreateIndex: true }
 mongoose
   .connect(uri, options)
@@ -43,6 +46,6 @@ app.use(history())
 app.use('/dist', express.static(path.join(__dirname, '/dist')))
 // app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(app.get('port'), function() {
-  console.log('Example app listening on port' + ' ' + app.get('port'))
+app.listen(port, function() {
+  console.log('Example app listening on port' + ' ' + port)
 })
